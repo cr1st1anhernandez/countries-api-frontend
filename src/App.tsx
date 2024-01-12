31658eb996344d5571c8945133c11e54322a7e3a
cristian <cristian0402218@gmail.com>
2024-01-12 12:20:00 -0600

    :wrench: Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import HomePage from './pages/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Countries Api</h1>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen keppel-light dark:kepple-dark">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,19 @@
+import { IoIosArrowForward } from "react-icons/io";
+import { Button } from "@nextui-org/react";
+
+export default function HomePage(): JSX.Element {
+  return (
+    <div className="min-h-screen flex flex-col max-w-[1500px] mx-auto xl:flex-row justify-between items-center p-6">
+      <article className="flex flex-col gap-4">
+        <h1>Countries Api</h1>
+        <p className="text-2xl text-zinc-500 font-bold text-pretty">
+          Your API to get curiosities about different countries.
+        </p>
+        <Button color="primary" className="font-bold text-lg w-fit" endContent={<IoIosArrowForward/>}>
+          View Docs
+        </Button>
+      </article>
+      <img src="/src/assets/principalImage.svg" width={600} height={600} alt="PrincipalImage" />
+    </div>
+  )
+}
